9798a0a2495ab401b22102aef1ffbd7eed18b98a
allyRx <herindrainyall2003@gmail.com>
2025-05-13 16:09:24 +0300

    fix the bug in the dependency

--- src/main/java/org/allyrx/avisuser/Service/userService.java
+++ src/main/java/org/allyrx/avisuser/Service/userService.java
@@ -7,7 +7,6 @@ import org.allyrx.avisuser.Entites.User;
 import org.allyrx.avisuser.Entites.Validation;
 import org.allyrx.avisuser.Enum.roleUser;
 import org.allyrx.avisuser.Repository.userRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
